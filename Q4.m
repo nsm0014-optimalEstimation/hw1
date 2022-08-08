df809b4aabe6b0d19cb184ddb8e098e2dd1f7126
Noah Miller <nsm0014@auburn.edu>
2022-08-08 14:15:08 -0500

    Finished q4 part c

--- Q4.m
+++ Q4.m
@@ -22,7 +22,25 @@ stem_y = con;
 plot_x = linspace(-5,5,101);
 plot_y = (1/(con_std*(2*pi)^0.5)).*exp(-0.5.*(((plot_x-con_avg)./con_std).^2));
 
+r = linspace(0,1.9,50);
+r_test = zeros(1, length(r));
+tests = zeros(1,1000);
+rand_rolls = @()sum(V(randperm(numel(V), 2)));
 
+for j = 1:length(r)
+    for q = 1:500
+        y_n = rand_rolls();
+        for i = 2:500
+            y_n = (1-r(j))*y_n+r(j)*rand_rolls();
+        end
+        tests(q) = y_n;
+    end
+r_test(j) = var(tests);
+end
+
+
+stem_x1 = r;
+stem_y1 = r_test;
 %% Plotting
 fig1 = figure('Position',[500 500 800 400]);
 title(sprintf('PDF Mean: %0.5g \t\t\t PDF STD: %0.5g',con_avg,con_std))
@@ -32,3 +50,11 @@ hold on
 stem(stem_x,stem_y)
 plot(plot_x,plot_y,LineWidth=2);
 saveas(fig1,sprintf('Q4_%s.png','a'))
+
+fig2 = figure('Position',[500 500 800 400]);
+xlabel('Roll','FontSize',16)
+ylabel('Variance','FontSize',16)
+hold on
+stem(stem_x1,stem_y1)
+xlim([0 1.9])
+saveas(fig2,sprintf('Q4_%s.png','c'))
